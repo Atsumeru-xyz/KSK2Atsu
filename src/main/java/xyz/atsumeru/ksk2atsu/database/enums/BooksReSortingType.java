85818ccf20ecf70a53be41da768b16bb5107fb08
AtsumeruDev <atsumeru.dev@gmail.com>
2024-01-03 18:05:08 +0200

    Add new Enums for resorting and migration type

---
+++ src/main/java/xyz/atsumeru/ksk2atsu/database/enums/BooksReSortingType.java
@@ -0,0 +1,9 @@
+package xyz.atsumeru.ksk2atsu.database.enums;
+
+/**
+ * Enum that indicates which type of result files sorting will be used: by publisher or by author
+ */
+public enum BooksReSortingType {
+    BY_PUBLISHER,
+    BY_AUTHOR
+}
---
+++ src/main/java/xyz/atsumeru/ksk2atsu/database/enums/MigrationType.java
@@ -0,0 +1,9 @@
+package xyz.atsumeru.ksk2atsu.database.enums;
+
+/**
+ * Enum that indicates which type of files migration will be used: move or copy
+ */
+public enum MigrationType {
+    MOVE,
+    COPY
+}
