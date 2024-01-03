85914ba3baba5e9d94d800d6dfc8d419a6ba34ed
AtsumeruDev <atsumeru.dev@gmail.com>
2024-01-03 18:05:55 +0200

    Improve BooksRenamer. Ability to resort Books/Doujins by Author

--- src/main/java/xyz/atsumeru/ksk2atsu/managers/BooksRenamer.java
+++ src/main/java/xyz/atsumeru/ksk2atsu/managers/BooksRenamer.java
@@ -3,23 +3,23 @@ package xyz.atsumeru.ksk2atsu.managers;
 import me.tongfei.progressbar.ProgressBar;
 import xyz.atsumeru.ksk2atsu.App;
 import xyz.atsumeru.ksk2atsu.database.Database;
-import xyz.atsumeru.ksk2atsu.database.enums.CatalogType;
+import xyz.atsumeru.ksk2atsu.database.enums.BooksReSortingType;
 import xyz.atsumeru.ksk2atsu.database.models.Book;
-import xyz.atsumeru.ksk2atsu.database.models.Content;
 import xyz.atsumeru.ksk2atsu.metadata.BookInfo;
 import xyz.atsumeru.ksk2atsu.metadata.FileMetadata;
 import xyz.atsumeru.ksk2atsu.metadata.YAMLContent;
-import xyz.atsumeru.ksk2atsu.utils.ComicUtils;
-import xyz.atsumeru.ksk2atsu.utils.ProgressBarBuilder;
-import xyz.atsumeru.ksk2atsu.utils.StringUtils;
+import xyz.atsumeru.ksk2atsu.utils.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BooksRenamer {
 
@@ -27,23 +27,23 @@ public class BooksRenamer {
      * Rename all archives in given input {@link File} directory creating new names from metadata and move Books from
      * {@link App#DOUJINS_FOLDER} into {@link App#BOOKS_FOLDER} if metadata for corresponding archive is in {@link Book} {@link Database} table
      *
-     * @param outputDir output {@link File} dir where sorted archives is stored
-     * @param database  link to {@link Database} object. Used for querying all data from {@link Book} table for later
-     *                  checking if corresponding archive is Book
+     * @param outputDir     output {@link File} dir where sorted archives is stored
+     * @param reSortingType if {@link BooksReSortingType#BY_AUTHOR}, files in output dir will be resorted by Author, otherwise by Publisher
      * @return {@link List} of {@link String} errors
      */
-    public static List<String> rename(File outputDir, Database database) {
+    public static List<String> rename(File outputDir, BooksReSortingType reSortingType) {
         List<FileMetadata> filesMetadata = MetadataParser.parse(outputDir);
-        Map<String, Content> booksByUrl = MetadataGenerator.getContentMapByUrl(database, CatalogType.BOOKS);
         ProgressBar progressBar = ProgressBarBuilder.create("Renaming files:", filesMetadata.size());
 
         List<String> errors = filesMetadata.stream()
                 .peek(fileMetadata -> progressBar.step())
-                .filter(fileMetadata -> !renameFile(fileMetadata, booksByUrl))
+                .filter(fileMetadata -> !renameFile(fileMetadata, reSortingType))
                 .map(FileMetadata::getFile)
                 .map(file -> "Unable to rename file: " + file)
                 .filter(StringUtils::isNotEmpty)
                 .collect(Collectors.toList());
+
+        cleanOutputFolder(outputDir);
 
         progressBar.close();
 
@@ -54,21 +54,38 @@ public class BooksRenamer {
      * Rename archive {@link File} using metadata info from {@link FileMetadata} by formula
      * <p>({@link BookInfo#getEvent()}) [{@link BookInfo#getAuthors()}] {@link BookInfo#getTitle()} (quality).cbz</p>
      *
-     * @param fileMetadata {@link FileMetadata} with {@link YAMLContent} and {@link BookInfo} metadata
-     * @param booksByUrl   {@link Map} with content url as key, and {@link Content} book as value for checking if archive
-     *                     will be moved into {@link App#BOOKS_FOLDER}
+     * @param fileMetadata  {@link FileMetadata} with {@link YAMLContent} and {@link BookInfo} metadata
+     * @param reSortingType if {@link BooksReSortingType#BY_AUTHOR}, files in output dir will be resorted by Author, otherwise by Publisher
      * @return true if rename/moving was successful
      */
-    private static boolean renameFile(FileMetadata fileMetadata, Map<String, Content> booksByUrl) {
+    private static boolean renameFile(FileMetadata fileMetadata, BooksReSortingType reSortingType) {
         try {
+            File parentDir = fileMetadata.getFile().getParentFile();
+            if (reSortingType == BooksReSortingType.BY_AUTHOR) {
+                parentDir = new File(
+                        parentDir.getParentFile(),
+                        Optional.ofNullable(fileMetadata.getBookInfo())
+                                .map(BookInfo::getAuthors)
+                                .filter(StringUtils::isNotEmpty)
+                                .map(ComicUtils::getArtistReplacedDeniedSymbols)
+                                .orElse(App.UNKNOWN)
+                );
+            }
+
             // It's book. Change directory from Doujins to Books
-            String parentDir = fileMetadata.getFile().getParentFile().toString();
-            if (booksByUrl.containsKey(fileMetadata.getBookInfo().getLink())) {
-                parentDir = parentDir.replace(App.DOUJINS_FOLDER, App.BOOKS_FOLDER);
+            boolean isBook = Optional.ofNullable(fileMetadata.getBookInfo())
+                    .map(BookInfo::getParodies)
+                    .filter(ArrayUtils::isNotEmpty)
+                    .map(parodies -> parodies.stream().anyMatch(parody -> StringUtils.equalsIgnoreCase(parody, App.ORIGINAL_WORK)))
+                    .orElse(false);
+
+            if (isBook) {
+                parentDir = new File(parentDir.toString().replace(App.DOUJINS_FOLDER, App.BOOKS_FOLDER));
             }
 
             // Get optional metadata values from book_info and file name (event and quality)
-            String event = Optional.ofNullable(fileMetadata.getBookInfo().getEvent())
+            String event = Optional.ofNullable(fileMetadata.getBookInfo())
+                    .map(BookInfo::getEvent)
                     .filter(StringUtils::isNotEmpty)
                     .map(value -> String.format("(%s) ", value))
                     .orElse("");
@@ -77,7 +94,28 @@ public class BooksRenamer {
             String quality = fileName.contains("x3200") || fileName.contains("x3199") || fileName.contains("x3100") ? " (x3200)" : "";
 
             // Create new file name using scheme: (event) [author] book name.cbz
-            File newFile = new File(parentDir, ComicUtils.getTitleReplacedDeniedSymbols(String.format("%s[%s] %s%s.cbz", event, fileMetadata.getBookInfo().getAuthors(), fileMetadata.getBookInfo().getTitle(), quality)));
+            String author = Optional.ofNullable(fileMetadata.getBookInfo())
+                    .map(BookInfo::getAuthors)
+                    .filter(StringUtils::isNotEmpty)
+                    .orElseGet(
+                            () -> Optional.ofNullable(fileMetadata.getYamlContent())
+                                    .map(YAMLContent::getArtist)
+                                    .filter(ArrayUtils::isNotEmpty)
+                                    .map(list -> list.get(0))
+                                    .orElse(App.UNKNOWN)
+                    );
+
+            String title = Optional.ofNullable(fileMetadata.getBookInfo())
+                    .map(BookInfo::getTitle)
+                    .filter(StringUtils::isNotEmpty)
+                    .orElseGet(
+                            () -> Optional.ofNullable(fileMetadata.getYamlContent())
+                                    .map(YAMLContent::getTitle)
+                                    .filter(StringUtils::isNotEmpty)
+                                    .orElseGet(() -> fileName.replace(".cbz", ""))
+                    );
+
+            File newFile = new File(parentDir, ComicUtils.getTitleReplacedDeniedSymbols(String.format("%s[%s] %s%s.cbz", event, author, title, quality)));
 
             // Create new potential dirs
             newFile.getParentFile().mkdirs();
@@ -86,7 +124,27 @@ public class BooksRenamer {
             Files.move(fileMetadata.getFile().toPath(), newFile.toPath());
             return true;
         } catch (Exception e) {
+            if (App.IS_DEBUG) {
+                e.printStackTrace();
+            }
             return e instanceof FileAlreadyExistsException;
+        }
+    }
+
+    /**
+     * Cleans output {@link File} directory by removing empty directories
+     *
+     * @param outputDir output {@link File} dir where sorted archives is stored
+     */
+    private static void cleanOutputFolder(File outputDir) {
+        try (Stream<Path> stream = Files.walk(outputDir.toPath())) {
+            stream.sorted(Comparator.reverseOrder())
+                    .map(Path::toFile)
+                    .filter(File::isDirectory)
+                    .filter(FileUtils::isDirectoryEmpty)
+                    .forEach(File::delete);
+        } catch (IOException e) {
+            System.err.println("Unable to delete directory!");
         }
     }
 }
